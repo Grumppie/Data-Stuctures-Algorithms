8ad189ad5cbab611816daf309db839f43caf099e
Sarthak Pawar <sarthakrajesh777@gmail.com>
2021-10-04 23:30:52 +0530

    Reversed the Array

--- Arrays/reverse-an-array.cpp
+++ Arrays/reverse-an-array.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+	int n;
+	cout<<"Enter the length of the array";
+	cin>>n;
+	
+	int arr[n];
+	cout<<"Enter all the "<<n<<" Elements of the array";
+	
+	for(int i =0;i<n;i++){
+		cin>>arr[i];
+	} 
 
-	cout<<"Hello World";
-	return 0;
+	cout<<"the reversed array is: \n";
+
+	for(int i =n-1;i>=0;i--){
+		cout<<arr[i]<<" ";
+	}
+
 }
